3ac16b6643f87e5daebc8ddaf321826e9f1f1893
Sorrow <sorrow@sanjuant.fr>
2022-11-25 15:08:15 +0100

    build: adapt for sanjuant

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,15 +1,12 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-export default () => ({
+export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: {
         rejectUnauthorized: false
       },
