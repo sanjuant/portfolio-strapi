99fd03edaa4920e32c8cb331aa2d378464e8fa64
sanjuant <sanjuan.thibaut@gmail.com>
2025-09-03 22:33:54 +0200

    build(docker): try to fix production env

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -3,5 +3,5 @@ export default ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://portfolio.sanjuant.fr'),
   app: { keys: env.array('APP_KEYS') },
-  proxy: { koa: true },
+  proxy: true,
 });
