e8faf08fa01aa800c6176f3e55b39444b9871e22
sanjuant <sanjuan.thibaut@gmail.com>
2025-09-03 17:24:24 +0200

    build(docker): set postgres SSL to false

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -7,9 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: {
-        rejectUnauthorized: false
-      },
+      ssl: env('DATABASE_SSL', false),
     },
     debug: false,
   },
